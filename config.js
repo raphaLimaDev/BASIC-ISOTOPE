67767bcfdfd201f1cb191d2204fb2c0b84e732ae
Raphael Lima <raphalima.dev@gmail.com>
2016-07-08 11:57:40 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,27 @@
+$(window).load(function(){
+    var $container = $('.filter-result');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+ 
+    $('.filter a').click(function(){
+        $('.filter .current').removeClass('current');
+        $(this).addClass('current');
+ 
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+         });
+         return false;
+    }); 
+});
